12474a8acc85509b4c93c996dccd768430cdb9b4
Alyssa Perez <aperez@censeohealth.com>
2019-10-27 19:51:56 -0500

    changed code

---
+++ clickygame/src/components/CardContainer.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import Card from './Cards';
+
+
+class CardContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            score: 1,
+
+
+        }
+    }
+
+};
+
+export default CardContainer;
---
+++ clickygame/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = props => (
+    <div className="container text-center" id="header-container">
+        <h1 id="logo"><b>The Clicky Game</b></h1>
+            <h5 className="subtitle">How to play:</h5>
+            <br>
+            </br>
+    <h5 className="subtitle"> Click an image to start! All images will shuffle after. Try to select each image only once! </h5>
+    </div>
+
+)
+export default Header;
