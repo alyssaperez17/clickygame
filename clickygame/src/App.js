dbac99fb9c49fd547f7b179cf770813bb11086a6
Alyssa Perez <aperez@censeohealth.com>
2019-10-30 16:12:36 -0500

    added images

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,14 +1,45 @@
 import React from 'react';
 import './App.css';
 import  Cards from "./components/Cards";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
-function App() {
-  return (
-    <div className="App">
-    <Cards />
-    </div>
-  );
+
+
+class App extends Component{
+  state ={
+    count: 0,
+    topScore: 0
+  }
+
 }
+
+updateCurrentScore = (newCount) => {
+  this.setState({newCount});
+  }
+
+  updateTopScore = (newTop) => {
+    if (newTop > this.state.topScore) {
+
+      this.setState({topScore: newTop - 1})
+    }
+
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar score={this.state.count} top={this.state.topScore}/>
+        <Header/>
+        <CardContainer updateCurrentScore={this.updateCurrentScore} updateTopScore={this.updateTopScore}/>
+      </div>
+    );
+  }
+}
+
+  
+
+
 
 export default App;
 
